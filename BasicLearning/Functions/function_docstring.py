e10d5832beb7992d7115aa28869278cc16136099
pranaypatel512 <www.pranay1337.com@gmail.com>
2017-08-12 23:59:12 +0530

    add[+]:  Added last example for python Functions

--- BasicLearning/Functions/function_docstring.py
+++ BasicLearning/Functions/function_docstring.py
@@ -1,8 +1,7 @@
 def print_max(x,y):
-    """ Prints maximum of two numbers
+    """ Prints maximum of two numbers.
 
-    Two values must be integers
-    """
+    Two values must be integers."""
     # Convert to int if possible
     x = int(x)
     y = int(y)
